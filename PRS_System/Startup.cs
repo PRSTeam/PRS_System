41aa8acf6a6f247e64bb9bc3efe7e4356447d94e
rujiraKASAMSUK <rujira.kas@ku.th>
2022-02-17 15:27:30 +0700

    commitของมนี

--- PRS_System/Startup.cs
+++ PRS_System/Startup.cs
@@ -50,7 +50,7 @@ namespace PRS_System
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
