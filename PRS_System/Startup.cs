90e640c27466a611a47df179f79d18bfdaa87091
Chinopas MUANGTHAI <chinopas.m@live.ku.th>
2022-02-17 15:21:59 +0700

    Commit startup เดะมณีแก้startupให้หน่อยนะ

--- PRS_System/Startup.cs
+++ PRS_System/Startup.cs
@@ -50,7 +50,7 @@ namespace PRS_System
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=FormPRS}/{action=Index}/{id?}");
             });
         }
     }
