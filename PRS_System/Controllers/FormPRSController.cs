808ae206fc75cdc4cf6f2b879480b5a14aa0daf2
Chinopas MUANGTHAI <chinopas.m@live.ku.th>
2022-02-17 14:51:22 +0700

    อัพโค้ดส่วนแก้หน้า Layoutเริ่มต้น และก็เพิ่ม Controller Form_PRS

---
+++ PRS_System/Controllers/FormPRSController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRS_System.Controllers
+{
+    public class FormPRSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
