8f25bebfc6e2b351e60b1d96177d5a8a3b6ade36
rujiraKASAMSUK <rujira.kas@ku.th>
2022-02-17 15:02:23 +0700

    อัพหน้า login

---
+++ PRS_System/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRS_System.Models;
+using OAuth2TestTool.MVC.Controllers;
+
+namespace PRS_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Authen()
+        {
+           var o2 = new OAuth2TestTool.MVC.Controllers.HomeController();
+           var kk = o2.Index("123456", "11111", false, false);
+        }
+    }
+}
