a68ce8454f55e66a099030d669c8adeba9c8c6cb
rujira KASAMSUK <rujira.kas@ku.th>
2022-02-23 15:59:26 +0700

    อัพหน้า information บางส่วน

---
+++ PRS_System/Controllers/InformationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRS_System.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            //if (HttpContext.Session.GetString("advisor-id") == null)
+            //{
+            //    ViewData["nav_manu"] = HttpContext.Session.GetString("advisor-id");
+            //}
+            //else if (HttpContext.Session.GetString("type-person") == "1" || HttpContext.Session.GetString("type-person") == "2")
+            //{
+            //    ViewData["nav_manu"] = HttpContext.Session.GetString("type-person").ToString();
+            //}
+            return View();
+        }
+
+        //public IActionResult GetSessionData(string txt)
+        //{
+        //    if (txt == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        HttpContext.Session.SetString("advisor-id", txt);
+        //        HttpContext.Session.SetString("type-person", "1");
+        //        return RedirectToAction("Index");
+        //    }
+        //}
+    }
+}
