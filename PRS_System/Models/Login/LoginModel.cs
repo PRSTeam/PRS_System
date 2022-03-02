4a317ca6fafb74fdd38500305a989ebe655e4c9c
rujira KASAMSUK <rujira.kas@ku.th>
2022-03-03 00:33:28 +0700

    อัพหน้าlogin

---
+++ PRS_System/Models/Login/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRS_System.Models.Login
+{
+    public class LoginModel
+    {
+        public class Sendlogin
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public class Receivelogin
+        {
+            public string user_id { get; set; }
+            public string name_th { get; set; }
+            public string name_eng { get; set; }
+            public string fullname_th { get; set; }
+            public string fullname_eng { get; set; }
+            public string position { get; set; }
+            public string user_type { get; set; }
+        }
+    }
+}
