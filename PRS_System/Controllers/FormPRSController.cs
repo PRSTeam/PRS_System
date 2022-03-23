276115bd6521fc35ba7e0378340ad6463ed5e025
Chinopas MUANGTHAI <chinopas.m@live.ku.th>
2022-03-23 16:39:06 +0700

    Revert "แก้รอบ2"
    This reverts commit b76eb330873309d2605fda9c0ba615538397948a.

--- PRS_System/Controllers/FormPRSController.cs
+++ PRS_System/Controllers/FormPRSController.cs
@@ -29,43 +29,147 @@ namespace PRS_System.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            string user_id = HttpContext.Session.GetString("uid").ToString();
+            indexmodel.ListForm= _formService.GetnamePRS(user_id);
+            return View(indexmodel);
 
         }
         public IActionResult form()
         {
+            //----Add Form Page
+            if(id_tor==0)
+            {
+                List<ProductDataModel> productDatas = new List<ProductDataModel>();
+                List<SubjectDataModel> subjectDatas = new List<SubjectDataModel>();
+                Createview.Productdata = _formService.GetValuesFormPRSProduct(id_tor);
+                Createview.Subjectdata = _formService.GetValuesFormPRSSubject(id_tor);
+                if(Createview.Productdata.Count==0)
+                {
+                    productDatas.Add(new ProductDataModel {
+                        Id_Product = 0
+                        ,
+                        NameProduct = ""
+                        ,
+                        Unit = ""
+                        ,
+                        AmtProduct = 0
+                        ,
+                        Price_Per_Piece = 0
+                        ,
+                        status="Open"
+                    }) ;
+                    Createview.Productdata = productDatas;
+                }
+                if(Createview.Subjectdata.Count==0)
+                {
+                    subjectDatas.Add(new SubjectDataModel
+                    {
+                        Id_Subject = 0
+                        ,
+                        Subject=""
+                        ,
+                        status="Open"
+                    });
+                    Createview.Subjectdata = subjectDatas;
+                }
+                
 
-            return View();
+                
+
+            }
+            //----Edit Form Page
+            else if(id_tor!=0)
+            {
+                double sumvalue = 0;
+                double vaxvalue = 0;
+                Createview = _formService.GetValuesFormPRS(id_tor);
+                Createview.Productdata = _formService.GetValuesFormPRSProduct(id_tor);
+                for(int i=0;i<Createview.Productdata.Count;i++)
+                {
+                    vaxvalue = (0.07) * (sumvalue + (Createview.Productdata[i].AmtProduct * Createview.Productdata[i].Price_Per_Piece));
+                    sumvalue = sumvalue + (Createview.Productdata[i].AmtProduct * Createview.Productdata[i].Price_Per_Piece);
+                }
+                Createview.vaxproduct = Math.Round(vaxvalue,2);
+                Createview.sumproduct = Math.Round(sumvalue +vaxvalue,2);
+                Createview.Subjectdata = _formService.GetValuesFormPRSSubject(id_tor);
+            }
+            Console.WriteLine("Check" + id_tor);
+            string user_id= HttpContext.Session.GetString("uid").ToString();       
+            Createview.FilePath = _accountService.GetSignature(user_id);
+            Createview.id_tor = id_tor;
+            return View(Createview);
         }
         [HttpPost]
-        public async Task<IActionResult> AddDataProcurement(CreateFormModel Procurement)
+        public async Task<IActionResult>  AddDataProcurement(CreateFormModel Procurement)
         {
             try
             {
                 //if (ModelState.IsValid)
                 //{
-                string uniquefile = null;
-                string filepath = null;
-                if (Procurement.FilePDF != null)
-                {
-                    string uploadfile = Path.Combine(_hostingEnvironment.WebRootPath, "File\\ListFormUser");
-                    string filename = Procurement.FilePDF.FileName;
-                    string[] fileextension = filename.Split(".");
-                    uniquefile = DateTime.Now.ToString("yyyyMMddHHmmss") + "." + fileextension[1];/* + "_" + editdata.ImagePicture.FileName*/
-                    filepath = Path.Combine(uploadfile, uniquefile);
-                    Procurement.FilePDF.CopyTo(new FileStream(filepath, FileMode.Create));
-                    Procurement.FilePath = uniquefile;
-                }
-                Procurement.User_ID = HttpContext.Session.GetString("USER_ID").ToString();
-                Console.WriteLine("Check");
+                    string uniquefile = null;
+                    string filepath = null;
+                    if (Procurement.FilePDF != null)
+                    {
+                        string uploadfile = Path.Combine(_hostingEnvironment.WebRootPath, "File\\ListFormUser");
+                        string filename = Procurement.FilePDF.FileName;
+                        string[] fileextension = filename.Split(".");
+                        uniquefile = DateTime.Now.ToString("yyyyMMddHHmmss") + "." + fileextension[1];/* + "_" + editdata.ImagePicture.FileName*/
+                        filepath = Path.Combine(uploadfile, uniquefile);
+                        Procurement.FilePDF.CopyTo(new FileStream(filepath, FileMode.Create));
+                        Procurement.FilePath = uniquefile;
+                    }
+                    Procurement.User_ID = HttpContext.Session.GetString("USER_ID").ToString();
+                    Console.WriteLine("Check");
                 //----แอดข้อมูลFormข้อมูลที่ไม่ได้เป้นลิสต์
                 _formService.AddFormDetailData(Procurement.FormDataDetail());
                 int id_tor = _formService.GetMaximumID_TOR();
-                //--------------------------------
-                _formService.AddProductData(Procurement.Productdata, id_tor);
-                _formService.AddSubjectData(Procurement.Subjectdata, id_tor);
+                    //--------------------------------
+                    _formService.AddProductData(Procurement.Productdata, Procurement.id_tor);
+                    _formService.AddSubjectData(Procurement.Subjectdata, Procurement.id_tor);
 
+                    
+                }
+                //----Edit Form to Database
+                else if(Procurement.id_tor != 0)
+                {
+                    // Delete Product กับ Subject ที่ต้องการจะลบ
+                    string[] listdelete_idProduct = Procurement.IndexProDelete.Split(",");
+                    for (int i = 0; i < listdelete_idProduct.Length; i++)
+                    {
+                        int id_productdelete = int.Parse(listdelete_idProduct[i]);
+                        if(id_productdelete!=0)
+                        {
+                            _formService.DeleteFormProductData(id_productdelete);
+                        }
+                        
+                    }
+                    string[] listdelete_idSubject = Procurement.IndexSubdelete.Split(",");
+                    for (int i = 0; i < listdelete_idSubject.Length; i++)
+                    {
+                        int id_productdelete = int.Parse(listdelete_idSubject[i]);
+                        if (id_productdelete != 0)
+                        {
+                            _formService.DeleteFormSubjectData(id_productdelete);
+                        }
+
+                    }
+                    //-----------------------------------------
+                    for (int i=0;i<=Procurement.Productdata.Count;i++)
+                    {
+                        if(Procurement.Productdata[i].Id_Product!=0)
+                        {
+                            
+                        }
+                        
+                        
+                        
+
+                    }
+                }
                 return Json(new { status = "success", Messege = "Add Complete" });
+                //if (ModelState.IsValid)
+                //{
+
                 //}
                 //else
                 //{
@@ -74,17 +178,17 @@ namespace PRS_System.Controllers
                 //                                             select error.ErrorMessage).ToList());
                 //    return Json(new { status = "error", detail = errorList, errorMessage = "Add fail" });
                 //}
-
+               
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return Json(new { status = "error", detail = ex.ToString(), errorMessage = "Have a problem while adding new performance testing" });
             }
+           
 
+            
 
-
-
-
+           
         }
         public IActionResult AddDataSuppies(CreateFormModel Suppies)
         {
--- PRS_System/IServices/IFormService.cs
+++ PRS_System/IServices/IFormService.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PRS_System.Models.Data;
+using PRS_System.Models.FormModel;
 namespace PRS_System.IServices
 {
     public interface IFormService
     {
         public void AddFormDetailData(FormPRSDataModel formdetaildata);
-        public void AddSubjectData(List<SubjectDataModel> formdetaildata, int ID_TOR);
+        public void AddSubjectData(List<SubjectDataModel>  formdetaildata,int ID_TOR);
         public void AddProductData(List<ProductDataModel> formdetaildata, int ID_TOR);
         public int GetMaximumID_TOR();
         public int GetMaximumID_PRODUCT_LIST();
         public int GetMaximumID_COM();
 
         public int GetMaximumID_ASSIST();
-
+        public List<FormPRSDataModel> GetnamePRS(string user_id);
         public int GetMaximumID_SUBJECT_LIST();
+        public FormPRSModel GetValuesFormPRS(int id_tor);
+        public List<ProductDataModel> GetValuesFormPRSProduct(int id_tor);
+        public List<SubjectDataModel> GetValuesFormPRSSubject(int id_tor);
+
+        public void EditFormDetailData(FormPRSDataModel formdetaildata);
+        public void EditFormProductlData(List<ProductDataModel> formdetaildata, int ID_TOR);
+        public void EditFormSubjectData(List<SubjectDataModel> formdetaildata, int ID_TOR);
+
+        public void DeleteFormProductData(int ID_Product);
+
+        public void DeleteFormSubjectData(int ID_Subject);
     }
 }
